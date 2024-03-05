99b56a0e0aad9d07a95c07a9419580f841a28af2
Konixy <konixy.p@gmail.com>
2024-03-05 15:56:46 +0100

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Options} */
+
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  printWidth: 160,
+  bracketSpacing: true,
+};
